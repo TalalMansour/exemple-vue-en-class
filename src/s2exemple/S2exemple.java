31c3ad4a6ad4e0b0417de4c6c886911538112393
Talal Mansour <Talal Mansour@TalalMansour-PC>
2018-09-17 17:39:01 -0400

    Ajoute JSONArray contien JSONObject de Course titre et resultat

--- src/s2exemple/S2exemple.java
+++ src/s2exemple/S2exemple.java
@@ -8,6 +8,7 @@ package s2exemple;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
 /**
  *
  * @author Talal Mansour
@@ -16,18 +17,39 @@ public class S2exemple {
     public static void main(String[] args) {
        try{ 
        String json = FileReader.loadFileIntoString("student.json", "UTF-8");
-       //System.out.printf(json);
+       //System.out.printinf(json);
        JSONObject mainObj = JSONObject.fromObject(json);
        int id = mainObj.getInt("student_id");
        String fName = mainObj.getString("first_name");
        String lName = mainObj.getString("last_name");
-       String dBrth = mainObj.getString("dat_birth");
+       String dBrth = mainObj.getString("date_birth");
        boolean active = mainObj.getBoolean("active");
        double gpa = mainObj.getDouble("GPA");
        
-
-// System.out.print( "\n "+ fName+ " " + id + "\n");
+       JSONArray jsonArr = mainObj.getJSONArray("results");
+       JSONObject singleResult; 
+       String course_id;
+       String course_title;
+       double mark;
        
+       //les sorties dans un array des object
+       JSONArray listCours = new JSONArray();
+       JSONObject  singleCours = new JSONObject();
+       for (int i = 0 ;i< jsonArr.size();i++){ 
+           singleResult = jsonArr.getJSONObject(i);
+           course_id = singleResult.getString("course_id");
+           course_title = singleResult.getString("course_title");
+           mark = singleResult.getDouble("mark");
+           singleCours.accumulate("titre",course_id );
+           singleCours.accumulate("resultat",mark );
+           listCours.add(singleCours);
+
+           System.out.print( "\n "+  singleCours + " " +"\n");     
+           System.out.print( "\n "+  listCours + " " +"\n");     
+           
+           singleCours.clear();
+           } 
+           System.out.print( "\n "+  listCours + " " +"\n");     
        
        
        
